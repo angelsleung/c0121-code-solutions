29df88295eaebf80d4f53921329a85443caa4e7a
Angel Leung <angelhleung@gmail.com>
2021-01-26 15:03:35 -0800

    Updated javascript-data-model based on feedback from Cody

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,15 +5,14 @@ var orderHistory = [
     orderTotal: 34,
     shipTo: 'JS Masher',
     orderNumber: 11439416898772232,
-    items:
-    [
+    deliveryDate: 20200808,
+    deliveryMethod: null,
+    items: [
       {
-        deliveryDate: 20200808,
-        deliveryMethod: null,
         name: 'JavaScript for impatient programmers',
         author: 'Ruschmayer, Dr. Axel',
-        returnCloseDate: 20200907,
-        price: 31.55
+        price: 31.55,
+        returnCloseDate: 20200907
       }
     ]
   },
@@ -23,15 +22,14 @@ var orderHistory = [
     orderTotal: 44.53,
     shipTo: 'JS Masher',
     orderNumber: 11399842681280257,
-    items:
-    [
+    deliveryDate: 20200720,
+    deliveryMethod: 'resident',
+    items: [
       {
-        deliveryDate: 20200720,
-        deliveryMethod: 'resident',
         name: 'The Timeless Way of Building',
         author: 'Alexander, Christopher',
-        returnCloseDate: 20200819,
-        price: 41.33
+        price: 41.33,
+        returnCloseDate: 20200819
       }
     ]
   },
@@ -41,15 +39,14 @@ var orderHistory = [
     orderTotal: 17.22,
     shipTo: 'JS Masher',
     orderNumber: 11428755579059409,
-    items:
-    [
+    deliveryDate: 20200707,
+    deliveryMethod: 'resident',
+    items: [
       {
-        deliveryDate: 20200707,
-        deliveryMethod: 'resident',
         name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         author: null,
-        returnCloseDate: 20200805,
-        price: 15.98
+        price: 15.98,
+        returnCloseDate: 20200805
       }
     ]
   },
@@ -59,23 +56,20 @@ var orderHistory = [
     orderTotal: 138.93,
     shipTo: 'JS Masher',
     orderNumber: 11328831772648248,
-    items:
-    [
+    deliveryDate: 20200705,
+    deliveryMethod: null,
+    items: [
       {
-        deliveryDate: 20200705,
-        deliveryMethod: null,
         name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         author: null,
-        returnCloseDate: 20200804,
-        price: 94.95
+        price: 94.95,
+        returnCloseDate: 20200804
       },
       {
-        deliveryDate: 20200705,
-        deliveryMethod: null,
         name: 'The Art of Sql',
-        author: null,
-        returnCloseDate: 20200804,
-        price: 33.99
+        author: 'Faroult, Stephane',
+        price: 33.99,
+        returnCloseDate: 20200804
       }
     ]
   }
