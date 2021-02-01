cb084e408e3e838e0374c2b828960577005cccb6
Angel Leung <angelhleung@gmail.com>
2021-02-01 10:40:10 -0800

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+var heading = document.querySelector('h1');
+console.log('Heading:', heading);
+console.dir(heading);
+
+var explanation = document.querySelector('#explanation');
+console.log('Explanation:', explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+console.log('Hint:', hint);
+console.dir(hint);
+
+var paragraphs = document.querySelectorAll('p');
+console.log('Paragraphs:', paragraphs);
+
+var links = document.querySelectorAll('.example-link');
+console.log('Example Links:', links);
