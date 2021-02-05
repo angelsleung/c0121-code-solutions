cd214400b4189da388797771499859fd357c5a3d
Angel Leung <angelhleung@gmail.com>
2021-02-04 23:00:58 -0800

    Completed working-with-collections

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,104 +1,105 @@
-// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+/* eslint-disable no-prototype-builtins */
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-// // Creates a collection of 52 card objects
-// var cards = [];
-// var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-// var suits = ['diamonds', 'clubs', 'hearts', 'spades'];
-// for (i = 0; i < 52; i++) {
-//   var card = {};
-//   card.rank = ranks[i % 13];
-//   card.suit = suits[Math.floor(i / 13)];
-//   cards.push(card);
-// }
+function createCards() {
+  var cards = [];
+  var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  var suits = ['diamonds', 'clubs', 'hearts', 'spades'];
+  for (var i = 0; i < 52; i++) {
+    var card = {};
+    card.rank = ranks[i % 13];
+    card.suit = suits[Math.floor(i / 13)];
+    cards.push(card);
+  }
+  return cards;
+}
 
-// function playGame(numPlayers, numCardsPerHand) {
+function createPlayers(numPlayers) {
+  var players = [];
+  for (var i = 0; i < numPlayers; i++) {
+    var player = {};
+    player.name = 'Player ' + (i + 1);
+    player.hand = [];
+    players.push(player);
+  }
+  return players;
+}
 
-//   // Creates a collection of player objects
-//   var players = [];
-//   for (var i = 0; i < numPlayers; i++) {
-//     var player = {};
-//     player.name = 'Player ' + (i + 1);
-//     player.hand = [];
-//     players.push(player);
-//   }
-//   var totalCards = numPlayers * numCardsPerHand;
+function shuffle(totalCards) {
+  while (cardIndexes.length <= totalCards) {
+    var cardIndex = Math.floor(Math.random() * 52);
+    if (!cardIndexes.includes(cardIndex)) {
+      cardIndexes.push(cardIndex);
+    }
+  }
+}
 
-//   // Creates an array of random (index) numbers between 0 and 51
-//   var randomNums = [];
-//   while (randomNums.length <= totalCards) {
-//     var randomNum = Math.floor(Math.random() * 52);
-//     if (!randomNums.includes(randomNum)) {
-//       randomNums.push(randomNum);
-//     }
-//   }
+function deal(totalCards, startIndex) {
+  for (var i = startIndex; i < totalCards; i++) {
+    var card = cards[cardIndexes[i]];
+    players[i % players.length].hand.push(card);
+  }
+}
 
-//   // Selects cards based on random numbers and deals to players in a circle
-//   for (i = 0; i < totalCards; i++) {
-//     card = cards[randomNums[i]];
-//     players[i % numPlayers].hand.push(card);
-//   }
+function score() {
+  for (var i = 0; i < players.length; i++) {
+    var score = 0;
+    var name = players[i].name;
+    var hand = players[i].hand;
+    for (var j = 0; j < hand.length; j++) {
+      var card = hand[j];
+      if (card.rank === 'Jack' || card.rank === 'Queen' || card.rank === 'King') {
+        score += 10;
+      } else if (card.rank === 'Ace') {
+        score += 11;
+      } else {
+        score += card.rank;
+      }
+    }
+    if (score > highScore) {
+      winners = [name];
+      highScore = score;
+    } else if (score === highScore) {
+      winners.push(name);
+    }
+  }
+}
 
-//   // Calculates each player's score and compares it to the high score
-//   var highScore = 0;
-//   var winners = [];
-//   for (i = 0; i < numPlayers; i++) {
-//     var score = 0;
-//     // var name = player[i].name;
-//     var hand = players[i].hand;
-//     for (var j = 0; j < numCardsPerHand; j++) {
-//       card = hand[j];
-//       if (card.rank === 'Jack' || card.rank === 'Queen' || card.rank === 'King') {
-//         score += 10;
-//       } else if (card.rank === 'Ace') {
-//         score += 11;
-//       } else {
-//         score += card.rank;
-//       }
-//       if (score >= highScore) {
-//         highScore = score;
-//         winners.push({ name: highScore });
-//       }
-//     }
-//   }
+function playGame(players, numCardsPerHand) {
+  var totalCards = players.length * numCardsPerHand;
+  shuffle(totalCards);
+  deal(totalCards, 0);
+  score();
+  if (winners.length > 1) {
+    handleTie(totalCards);
+  }
+  var winnerName = winners[0];
+  console.log('The winner is', winnerName + '!');
+  console.log('Players:', players);
+}
 
-//   // To determine the winner in the case of a tie
-//   var newRandomNums = [];
-//   while (newRandomNums.length <= winners.length) {
-//     var newRandomNum = Math.floor(Math.random() * 52);
-//     if (!newRandomNums.includes(newRandomNum)) {
-//       newRandomNums.push(newRandomNum);
-//     }
-//   }
-//   for (i = 0; i < winners.length; i++) {
+function handleTie(totalCards) {
+  while (winners.length > 1) {
+    console.log('Tie - deal another card as tie-breaker');
+    var tiePlayers = [];
+    for (var i = 0; i < players.length; i++) {
+      if (winners.includes(players[i].name)) {
+        tiePlayers.push(players[i]);
+      }
+    }
+    players = tiePlayers;
+    var dealStartIndex = totalCards;
+    totalCards += winners.length;
+    shuffle(totalCards);
+    deal(totalCards, dealStartIndex);
+    winners = [];
+    score();
+  }
+}
 
-//     // winner[i];
-//     card = cards[newRandomNums[i]];
-//     if (card.rank === 'Jack' || card.rank === 'Queen' || card.rank === 'King') {
-//       score += 10;
-//     } else if (card.rank === 'Ace') {
-//       score += 11;
-//     } else {
-//       score += card.rank;
-//     }
-//     if (score >= highScore) {
-//       highScore = score;
-//       winners.push({ name: highScore });
-//     }
-
-//     winners[i % numPlayers].hand.push(card);
-
-//   }
-
-//   // var highScore = 0;
-//   // var winners = [];
-//   // for (i = 0; i < winners; i++) {
-//   //   var score = 0;
-//   //   var name = player[i].name;
-//   //   var hand = players[i].hand;
-//   //   card = hand[j];
-//   // }
-
-//   // console.log('The winner is', winner + '!');
-// }
-
-// playGame(4, 2);
+var cardIndexes = [];
+var winners = [];
+var highScore = 0;
+var cards = createCards();
+var players = createPlayers(4);
+playGame(players, 2);
