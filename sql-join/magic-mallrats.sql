29963082fea4ddd651b6d82d23067de7d5eb04fe
Angel Leung <angelhleung@gmail.com>
2021-03-18 11:02:53 -0700

    Removed extra semicolon from magic-mallrats.sql

--- sql-join/magic-mallrats.sql
+++ sql-join/magic-mallrats.sql
@@ -1,7 +1,7 @@
 select "customers"."firstName",
   "customers"."lastName"
   from "customers"
-  join "rentals" using ("customerId");
+  join "rentals" using ("customerId")
   join "inventory" using ("inventoryId")
   join "films" using ("filmsId")
   where "films"."title" = 'Magic Mallrats';
