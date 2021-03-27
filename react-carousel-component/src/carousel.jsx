69489148a241ebe55043c715af32d9597f1b2d3f
Angel Leung <angelhleung@gmail.com>
2021-03-26 23:59:38 -0700

    Added functionality to enable clicking the arrows to view the next or previous image

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,15 +3,33 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { display: 'pikachu' };
+    this.state = { displayIndex: 0 };
+    this.stepRight = this.stepRight.bind(this);
+    this.stepLeft = this.stepLeft.bind(this);
+  }
+
+  stepRight() {
+    if (this.state.displayIndex === this.props.images.length - 1) {
+      this.setState({ displayIndex: 0 });
+    } else {
+      this.setState({ displayIndex: this.state.displayIndex + 1 });
+    }
+  }
+
+  stepLeft() {
+    if (this.state.displayIndex === 0) {
+      this.setState({ displayIndex: this.props.images.length - 1 });
+    } else {
+      this.setState({ displayIndex: this.state.displayIndex - 1 });
+    }
   }
 
   render() {
     return (
     <div className='carousel'>
-      <i className="arrow fas fa-chevron-left"></i>
+      <i className="arrow fas fa-chevron-left" onClick={this.stepLeft}></i>
       <div className='image-dots'>
-        <img className='image' src='../images/025.png' alt='pikachu'></img>
+        <img className='image' src={this.props.images[this.state.displayIndex]} alt='pikachu'></img>
         <div className='dots'>
           <i className="dot fas fa-circle"></i>
           <i className="dot far fa-circle"></i>
@@ -20,7 +38,7 @@ class Carousel extends React.Component {
           <i className="dot far fa-circle"></i>
         </div>
       </div>
-      <i className="arrow fas fa-chevron-right"></i>
+      <i className="arrow fas fa-chevron-right" onClick={this.stepRight}></i>
     </div>
     );
   }
